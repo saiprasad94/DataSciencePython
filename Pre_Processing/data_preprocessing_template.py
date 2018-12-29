6e412adabc63bb4a5a04771dd3b790e494754218
PRASAD_BR160F <sashila@mail.bradley.edu>
2018-12-29 04:04:55 -0600

    Primary acummialtion pushed
    Initial work acummilated puhed : commit1

---
+++ Pre_Processing/data_preprocessing_template.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 23 02:46:10 2018
+
+@author: sashila
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the data
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:,3].values
+
+# Splitting the dataset into the training set and test set
+from sklearn.model_selection import train_test_split
+X_train , X_test , Y_train , Y_test = train_test_split(X,Y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
