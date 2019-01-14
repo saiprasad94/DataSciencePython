aa3e2b706c30283e9897c5ca4a629d6c85d4bf66
BR160ASP <sashila@ad.bradley.edu>
2019-01-14 12:29:32 -0600

    tt dd

--- Multiple_Linear_Regression/multiple_linear_regression.py
+++ Multiple_Linear_Regression/multiple_linear_regression.py
@@ -5,7 +5,10 @@ import numpy as np
 
 import pandas as pd
 
-
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
 
 
 
